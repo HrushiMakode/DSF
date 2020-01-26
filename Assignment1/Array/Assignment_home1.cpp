29f31069b1150862cae767825c9777c1b879fd3a
HrushiMakode <makodehrushikesh@gmail.com>
2020-01-26 20:38:29 +0530

    All updated

--- Assignment1/Array/Assignment_home1.cpp
+++ Assignment1/Array/Assignment_home1.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Assignment1.cpp
-// Author      : Gaurav
+// Author      : Hrushikesh 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
--- Assignment1/LinkedList/Assignment1.cpp
+++ Assignment1/LinkedList/Assignment1.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Assignment1.cpp
-// Author      : Gaurav
+// Author      : Hrushikesh Makode
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
